522c7f1eab571f12e070974880d3ae94f0f0578e
dani <daniboo83@gmail.com>
2018-07-24 17:32:10 +0100

    Adding HTML to ruby files via Sinatra

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Intro to the web
 
+## Playing with Sinatra (and shotgun!)
+
 ![alt text](http://res.cloudinary.com/dani-devs-and-designs/image/upload/v1532448077/Frank_Sinatra_a4tnuj.png)
--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
+require 'shotgun'
+
+# set :session_secret, 'super secret'
 
 get '/' do
-  "hello"
+  "baaah!!"
 end
 
 get '/secret' do
   "these aren't the droids you're looking for"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE' style='border: 3px dashed red;'>
+  </div>"
 end

b0eaf1df1759e241749fdf92a4ba13ba9b71243d
dani <daniboo83@gmail.com>
2018-07-25 11:13:27 +0100

    Added parameters

--- app.rb
+++ app.rb
@@ -11,7 +11,12 @@ get '/secret' do
   "these aren't the droids you're looking for"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking", "Mr Fluffywinks"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end

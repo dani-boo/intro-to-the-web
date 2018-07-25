35cefa18ab770474c28abb49d1a5c105989e1af1
dani <daniboo83@gmail.com>
2018-07-25 12:58:39 +0100

    Extraction

--- app.rb
+++ app.rb
@@ -13,11 +13,15 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking", "Mr Fluffywinks"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
